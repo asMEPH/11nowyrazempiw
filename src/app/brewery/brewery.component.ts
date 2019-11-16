ec820d1a2bab452c39ed3af863aaacf632b53015
Your Name <you@example.com>
2019-11-16 11:07:47 +0100

    asfdfgd

--- src/app/brewery/brewery.component.ts
+++ src/app/brewery/brewery.component.ts
@@ -10,19 +10,42 @@ import {IngredientService} from "../ingredient.service";
 export class BreweryComponent implements OnInit {
   beerQuantity: 0;
   progress = 5;
+  orderedUnits = 0;
+
 
   constructor(public brewService: BrewService,
               public ingredientService: IngredientService) {
 
+
   }
 
   ngOnInit() {
+
   }
+
 
   percent_of_ingredient(ingName: string) {
     let x = this.ingredientService.ingredients.get(ingName);
     let percent = (x / 200) * 100;
     return percent + '%';
+  }
+
+  prct(value: number, max_value: number): string {
+    return(value / max_value) * 100 + '%';
+  }
+
+
+
+
+
+
+  exmpl(){
+    this.orderedUnits = this.orderedUnits + 1;
+
+  }
+  exmpl2(){
+    this.orderedUnits = this.orderedUnits - 1;
+    if(this.orderedUnits = 0){ this.orderedUnits = 0;}
   }
 
 }
